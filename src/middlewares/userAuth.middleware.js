74c71e01931fe618b5f0b4755574780eadfdbf0a
Harsh <shivhareharsh027@gmail.com>
2026-09-22 23:38:12 +0530

    make middileware of user

---
+++ src/middlewares/userAuth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler( async(req, res, next) => {
+    try{
+        // take token
+        const token = req.cookies?.accessToken || 
+        req.header("Authorization")?.replace("Bearer ", "");
+        
+       // validate token exist or not
+        if(!token){
+            throw new ApiError(401, "Unauthorized access");
+        }
+        // token varify 
+        const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        // user from token 
+        const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
+
+        if(!user) {
+            throw new ApiError("401", "Invalid access token")
+        }
+
+        req.user = user;
+        next();
+    } 
+    catch(error) {
+        throw new ApiError(401, error?.message || "Invalide access token")
+    }
+})
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,9 @@
+/**
+ * Higher-order function jo async controller ke errors ko
+ * Express ke GLOBAL error handler tak forward karta hai.
+ * Isse poore API ka error response shape ek jaisa rehta hai.
+ */
+const asyncHandler = (fn) => (req, res, next) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
 
-
-const asyncHandler = (fn) => async (req, res, next) => {
-    try{
-        await fn(req, res, next)
-    } catch(err) {
-        res.status(err.statusCode || 500).json({
-            success : false,
-            message : err.message
-        })
-    }
-}
-
-export {asyncHandler} 
+export { asyncHandler };

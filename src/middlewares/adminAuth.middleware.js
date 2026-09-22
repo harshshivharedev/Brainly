6e807002df2a9e922bedca735b60bc0eb0fabc01
Harsh <shivhareharsh027@gmail.com>
2026-09-22 23:39:19 +0530

    make auth middleware for admin

---
+++ src/middlewares/adminAuth.middleware.js
@@ -0,0 +1,33 @@
+
+import jwt from "jsonwebtoken";
+
+import { ApiError } from "../utils/apiError.js";
+import { Admin } from "../models/admin.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler( async(req, res, next) => {
+    try{
+        // take token
+        const token = req.cookies?.accessToken || 
+        req.header("Authorization")?.replace("Bearer ", "");
+        
+       // validate token exist or not
+        if(!token){
+            throw new ApiError(401, "Unauthorized access");
+        }
+        // token varify 
+        const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        // user from token 
+        const admin = await Admin.findById(decodeToken?._id).select("-password -refreshToken")
+
+        if(!admin) {
+            throw new ApiError("401", "Invalid access token")
+        }
+
+        req.Admin = admin;
+        next();
+    } 
+    catch(error) {
+        throw new ApiError(401, error?.message || "Invalide access token")
+    }
+})
